5279aa4387fcf2fc1d37667d0cd445f1a755fd14
alantop <alantop@qq.com>
2017-10-06 15:16:28 +0800

    delete ssj e-mail

--- navigation.js
+++ navigation.js
@@ -1,3 +1,3 @@
 // JavaScript Document
 
-document.write('<p><a href="index.html">index<a>   <a href="bank.html">bank</a> <a href="p2p.html">p2p</a> <a href="soft.html">soft</a> <a href="http://money08.feidee.com/u08/report_index.do">feidee</a> 2956277043@qq.com</p>');
+document.write('<p><a href="index.html">index<a>   <a href="bank.html">bank</a> <a href="p2p.html">p2p</a> <a href="soft.html">soft</a> <a href="http://money08.feidee.com/u08/report_index.do">feidee</a></p>');
