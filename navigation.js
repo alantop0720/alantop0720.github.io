b3f7fd610e3cfe95b54522e276dddc34af90a82e
alantop0720 <alantop@qq.com>
2018-03-01 14:04:07 +0800

    update

--- navigation.js
+++
@@ -1,3 +0,0 @@
-// JavaScript Document
-
-document.write('<p><a href="index.html">index<a>   <a href="bank.html">bank</a> <a href="p2p.html">p2p</a> <a href="soft.html">soft</a> <a href="https://www.sui.com/report_index.do">feidee</a></p>');
