2d4af1dbcda40ae64f183b27546d85434e6c5391
alantop0720 <30796007@qq.com>
2024-04-22 21:19:50 +0800

    delete git note

--- git_note/init.bat
+++
@@ -1,9 +0,0 @@
-git config --global user.name ""
-git config --global user.email  ""
-git config --global credential.helper store
-
-git clone qian
-git config --global http.sslVerify false
-
-
-git remote set-url origin https://token@github.com/alantop0720/alantop0720.github.io.git
--- git_note/push.bat
+++
@@ -1,7 +0,0 @@
-chcp 65001
-git add .
-git status
-git commit -m "normal update"
-git push
-
-pause
